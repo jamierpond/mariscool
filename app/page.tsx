78e2390243fd25c830013564fb586825ab0fe855
Marianne Rotyliano <mariannerotyliano@Mariannes-Air-2.lan>
2024-01-01 20:33:18 -0800

    fun stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Mar from "../public/mar.jpg";
+import Cookie from "../public/cookie.png";
+
 import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>hello mar</p>
-      <Image src={Mar} alt="Mar" />
+      <p>hey jamie</p>
+      <Image src={Mar} width={400} alt="Mar" />
+      <Image src={Cookie} width={400} alt="Cookie" />
+      <video width="320" height="240" controls>
+        <source src="/jamieskating.MOV" type="video/mp4" />
+      </video>
     </main>
   );
 }
