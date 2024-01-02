91618ed1edef7edfbea27b75a7fe5a0d0b5b18e6
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-01-01 20:33:12 -0800

    update some stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Mar from "../public/mar.jpg";
+import Cookie from "../public/cookie.png";
+
 import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>hello mar</p>
-      <Image src={Mar} alt="Mar" />
+      <p>i am a lemon</p>
+      <Image src={Mar} width={500} alt="Mar" />
+      <Image src={Mar} width={500} alt="Mar" />
+      <img src="/mar.jpg" alt="Jamie" />
     </main>
   );
 }
