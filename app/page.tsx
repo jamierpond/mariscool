5a26b770d7c8e207cba9fce7ca60e6bd71d737e6
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-01-01 18:51:49 -0800

    add img

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Mar from "../public/mar.jpg";
+import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>hello mar</p>
+      <Image src={Mar} alt="Mar" />
     </main>
   );
 }
