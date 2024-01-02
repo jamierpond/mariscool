35daba1b6b87a33061d18343a8b830bf76bf9ed8
Marianne Rotyliano <mariannerotyliano@Mariannes-Air-2.lan>
2024-01-01 20:49:21 -0800

    green fun

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@ import Mar from "../public/mar.jpg";
 import Cookie from "../public/cookie.png";
 
 import Image from "next/image";
+
 export default function Home() {
+  const containerStyle = {
+    backgroundColor: "forestgreen",
+    fontFamily: "Comic Sans MS, cursive",
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main style={containerStyle} className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>hey jamie</p>
       <Image src={Mar} width={400} alt="Mar" />
       <Image src={Cookie} width={400} alt="Cookie" />
